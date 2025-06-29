e32e5a17d0d3c8c97ccbd69e05aedabe0a21294f
YanBerdin <115979885+YanBerdin@users.noreply.github.com>
2025-06-30 01:15:33 +0200

    Fix metadata export error by moving to layout

--- app/compagnie/page.tsx
+++ app/compagnie/page.tsx
@@ -1,15 +1,5 @@
-import { Metadata } from 'next';
 import CompagnieClientPage from './client-page';
 
-export const metadata: Metadata = {
-  title: 'La Compagnie | Rouge-Cardinal',
-  description: 'Découvrez l\'histoire, la mission et l\'équipe de la compagnie de théâtre Rouge-Cardinal.',
-};
-
 export default function CompagniePage() {
-  return (
-    <div>
-      <CompagnieClientPage />
-    </div>
-  );
+  return <CompagnieClientPage />;
 }
